a69c6fd296c44c64c0c45781c08ef54d23eb94f4
Alper Alkan <22225051+Alfagun74@users.noreply.github.com>
2020-11-14 04:25:45 +0100

    linter removed

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as fs from 'fs';
-import fetch from 'node-fetch';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import cheerio from 'cheerio';
-import {Video} from './models/video';
-import puppeteer from 'puppeteer-extra';
+import * as fs from "fs";
+import fetch from "node-fetch";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import cheerio from "cheerio";
+import { Video } from "./models/video";
+import puppeteer from "puppeteer-extra";
 puppeteer.use(StealthPlugin());
 puppeteer.use(
-  require('puppeteer-extra-plugin-block-resources')({
-    blockedTypes: new Set(['image', 'stylesheet', 'font']),
+  require("puppeteer-extra-plugin-block-resources")({
+    blockedTypes: new Set(["image", "stylesheet", "font"]),
   })
 );
 
 export async function vivodl(
-  destinationFolder = '',
+  destinationFolder = "",
   vivoUrls: string[] = []
 ): Promise<Video[]> {
   let videos: Video[] = [];
@@ -26,38 +26,38 @@ export async function vivodl(
 }
 
 async function fetchVideoSources(vivoUrls: string[]): Promise<Video[]> {
-  console.log('▶ Fetching all video sources');
+  console.log("▶ Fetching all video sources");
   const videos: Video[] = [];
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const context = await browser.createIncognitoBrowserContext();
   await Promise.all(
     vivoUrls.map(async (vivoUrl: string) => {
       const page = await context.newPage();
       await page.goto(vivoUrl);
       await Promise.all([
-        page.waitForSelector('div.stream-content'),
-        page.waitForSelector('source'),
+        page.waitForSelector("div.stream-content"),
+        page.waitForSelector("source"),
       ]);
       const $ = cheerio.load(
         await page.evaluate(() => document.body.innerHTML)
       );
       await page.close();
-      const extractedName = $('div.stream-content').attr('data-name')!;
-      const extractedUrl = $('source').attr('src')!;
+      const extractedName = $("div.stream-content").attr("data-name")!;
+      const extractedUrl = $("source").attr("src")!;
       videos.push(new Video(extractedName, extractedUrl, vivoUrl));
     })
   );
   await browser.close();
-  console.log('✔ Fetched all video sources');
+  console.log("✔ Fetched all video sources");
   return videos;
 }
 
 async function downloadVideos(videos: Video[], destinationFolder: string) {
   if (!destinationFolder) {
-    console.log('✨ Fetched the video URIs}');
+    console.log("✨ Fetched the video URIs}");
     return;
   }
-  console.log('▶ Starting video downloads');
+  console.log("▶ Starting video downloads");
   destinationFolder = stripPath(destinationFolder);
   await Promise.all(
     videos.map(async video => {
@@ -82,5 +82,5 @@ async function downloadVideos(videos: Video[], destinationFolder: string) {
 }
 
 function stripPath(path: string): string {
-  return path.replace(/\/$/, '');
+  return path.replace(/\/$/, "");
 }
